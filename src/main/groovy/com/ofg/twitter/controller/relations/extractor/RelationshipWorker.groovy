7bc233fcf7ce8eb497ed602c922ec7f70c7b9cdd
Michał Lewandowski <mlw@touk.pl>
2014-08-09 18:40:31 +0200

    Should send now

--- src/main/groovy/com/ofg/twitter/controller/relations/extractor/RelationshipWorker.groovy
+++ src/main/groovy/com/ofg/twitter/controller/relations/extractor/RelationshipWorker.groovy
@@ -4,6 +4,8 @@ import com.ofg.infrastructure.discovery.ServiceResolver
 import com.ofg.infrastructure.web.resttemplate.RestTemplate
 import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
 import org.springframework.stereotype.Component
 
 @Slf4j
@@ -21,8 +23,13 @@ class RelationshipWorker {
     void passRelations(String request) {
         try{
             String adress = serviceResolver.getUrl("gui").get() + "/rest/results/result"
-            log.error("Adress: $adress and request: $request")
-            restTemplate.putForObject(adress, request, String)
+            log.debug("Adress: $adress and request: $request")
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("Content-Type", "application/vnd.gui.v1+json");
+
+            HttpEntity<String> entity = new HttpEntity<String>(request,headers);
+
+            restTemplate.put(adress, entity)
         } catch(Exception e){
             log.error("Catch log", e)
         }
